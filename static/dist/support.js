42ea51c35e6433489ef9fce98700e2081cabc3a8
marlon-erler <viridian035@gmail.com>
2024-07-07 15:48:23 +0200

    update carbon-mini

--- static/dist/support.js
+++ static/dist/support.js
@@ -1,3 +1,4 @@
+// TABS
 document.querySelector("main").addEventListener("scroll", () => {
   const links = document.querySelectorAll(".tab-link");
   links.forEach((element) => element.removeAttribute("active"));
@@ -8,3 +9,18 @@ document.querySelector("main").addEventListener("scroll", () => {
 
   if (links[currentPage]) links[currentPage].setAttribute("active", "");
 });
+
+// SERVICE WORKER
+async function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+    } catch (error) {
+      console.error(`Could not install service worker: ${error}`);
+    }
+  }
+}
+
+registerServiceWorker();
---
+++ static/dist/sw.js
@@ -0,0 +1,38 @@
+const CACHE_VERSION = "v1";
+
+async function handleRequest(event) {
+  const responsePromise = getResponse(event);
+  event.respondWith(responsePromise);
+}
+
+async function getResponse(event) {
+  try {
+    return await fetchAndCache(event);
+  } catch {
+    return await getFromCache(event);
+  }
+}
+
+async function fetchAndCache(event) {
+  const request = event.request;
+
+  const response = await fetch(request);
+
+  const cache = await caches.open(CACHE_VERSION);
+  await cache.put(request, response.clone());
+
+  return response;
+}
+
+async function getFromCache(event) {
+  const request = event.request;
+
+  const cache = await caches.open(CACHE_VERSION);
+  const response = await cache.match(request);
+
+  return response;
+}
+
+self.addEventListener("fetch", (event) => {
+  handleRequest(event);
+});
