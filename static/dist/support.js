fb6a1da62a40fc81ba83e679cc6e695652263fd9
marlon-erler <viridian035@gmail.com>
2024-07-07 17:33:20 +0200

    update service worker

--- static/dist/support.js
+++ static/dist/support.js
@@ -14,9 +14,14 @@ document.querySelector("main").addEventListener("scroll", () => {
 async function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
     try {
-      await navigator.serviceWorker.register("/sw.js", {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
         scope: "/",
       });
+      if (registration.installing) {
+        setTimeout(() => window.location.reload(), 500);
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
     } catch (error) {
       console.error(`Could not install service worker: ${error}`);
     }
